c250c4fa4808ab42ad28a079970358f66aed9926
noahtkeller <nkeller@mobiquityinc.com>
2017-01-27 19:42:47 -0500

    Closes simonlast/node-persist#76
    modified:   src/local-storage.js
    
    * Call interval.unref() after setting interval

--- src/local-storage.js
+++ src/local-storage.js
@@ -665,6 +665,7 @@ LocalStorage.prototype = {
     startPersistInterval: function (persistFunction) {
         this.stopPersistInterval();
         this._persistInterval = setInterval(persistFunction || this.persist.bind(this), this.options.interval);
+        this._persistInterval.unref();
     },
 
     stopPersistInterval: function () {
@@ -674,6 +675,7 @@ LocalStorage.prototype = {
     startExpiredKeysInterval: function () {
         this.stopExpiredKeysInterval();
         this._expiredKeysInterval = setInterval(this.removeExpiredItems.bind(this), this.options.expiredInterval);
+        this._expiredKeysInterval.unref();
     },
 
     stopExpiredKeysInterval: function () {
